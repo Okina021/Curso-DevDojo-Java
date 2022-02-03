97a9efa2304417516c43674dbfb12ecef00223d3
Gustavo <gustavo.amaral2310@gmail.com>
2022-02-03 19:28:33 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * isso é um comentário <b>isso é um comentário</b>
+ */
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Scanner;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.next();
+        System.out.print("Digite seu endereço: ");
+        String endereco = scanner.next();
+        System.out.print("Digite seu salario: ");
+        scanner.nextLine();
+        float salario = scanner.nextFloat();
+        System.out.print("Digite a data (dd/mm/aa): ");
+        String data = scanner.next();
+        System.out.print("Eu " + nome + ", morando no endereço " + endereco + "\nconfirmo que recebi o salário de " + salario + ", no data de " + data+".");
+        System.out.print("\nConfirma o Recebimento? (S/N): ");
+        char confirmacao = scanner.next().charAt(0);
+        if (confirmacao == 'S' || confirmacao == 's') {
+            System.out.println("Salario confirmado");
+        } else {
+            System.out.println("Salario nao confirmado");
+
+        }
+    }
+}
